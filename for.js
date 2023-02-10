9eb07437db58db8e0f8f5c601aa9b7d89a6ad9a8
Vitor Luiz Ramalho <vitorlramalho@gmail.com>
2023-02-10 13:52:30 -0300

    commit

---
+++ for.js
@@ -0,0 +1,10 @@
+const numeros = [100, 200, 300, 400, 500, 600];
+
+// 1 Primeira expressão: é executada apenas uma única vez
+// 2 Segunda expressão: condição de execução
+// 3 Terceira expressão: é executada sempre ao final do bloco
+
+//        1          2          3 
+for(let index = 0; index < 6; index++ ){
+    console.log("oi");
+}
