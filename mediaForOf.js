a7940af89b4bfbe279f0abe089be732ea24b2c50
Vitor Luiz Ramalho <vitorlramalho@gmail.com>
2023-02-13 10:51:38 -0300

    commit

--- mediaForOf.js
+++ mediaForOf.js
@@ -1,10 +1,9 @@
 const notas = [10, 6.5, 8, 7.5];
 let somaDasNotas = 0;
 
-for(let i = 0; i < notas.length; i++ ){
- somaDasNotas += notas[i];
-};
+for(let nota of notas){
+    somaDasNotas += nota;
+}
 
 const media = somaDasNotas / notas.length;
-
-console.log(media);
+console.log(media)
